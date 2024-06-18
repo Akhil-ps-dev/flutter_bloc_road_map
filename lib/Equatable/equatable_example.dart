17a1d3895516f4f5b158e244b59798eae893230e
Akhil-ps-dev <psakhil211@gmail.com>
2024-06-18 12:39:45 +0530

    updated

--- lib/Equatable/equatable_example.dart
+++ lib/Equatable/equatable_example.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 
 class EquatableTesting extends StatelessWidget {
@@ -5,6 +6,28 @@ class EquatableTesting extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        //equatable example code
+        
+        Person p1 = Person(name: 'John', age: 30);
+        Person p2 = Person(name: 'John', age: 30);
+        print(p1.hashCode.toString() == p2.hashCode.toString());
+        print(p1.hashCode.toString());
+        print(p2.hashCode.toString());
+      },
+      child: const Icon(Icons.add),
+    ));
   }
+}
+
+class Person extends Equatable {
+  final String name;
+  final int age;
+
+  Person({required this.name, required this.age});
+
+  @override
+  List<Object?> get props => [name, age];
 }
